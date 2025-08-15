59cd1201c5d0859d7e5f1ae5c22828169577b14f
aboveheavenfeels <arrogantejhoncarlo@gmail.com>
2025-08-15 09:12:29 +0000

    Act5.dart

---
+++ Act5.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+// void main () {
+//   print ("Enter your Age:");
+//   int? Age = int.parse(stdin.readLineSync()!);
+//    print("Your age is ${Age}");
+   
+// }
+
+// void main () {
+//   print("Enter your Age:");
+//   double? Age = double.parse(stdin.readLineSync()!);
+
+//   print("Your age is ${Age}");
+// }
+
+void main () {
+  print("Enter 1st Num");
+  double? num1 = double.parse(stdin.readLineSync()!);
+
+  print("Enter 2st Num");
+  double? num2= double.parse(stdin.readLineSync()!);
+
+  print("Enter 3st Num");
+  double? num3 = double.parse(stdin.readLineSync()!);
+
+  print("Enter 4th Num");
+  double? num4 = double.parse(stdin.readLineSync()!);
+
+  print("1st num + 3rd num + 2nd num * 1st num ${num1 + num3 + num2}");
+  print("2nd num - 3rd num / 1st num ${num2 + num3 + num1}");
+  print("4th num + 1st num * 4th num ${num4 + num1 + num4}");
+  print("1st num / 2nd num + 3rd num * 4th num ${num1 + num2 + num3 + num4}");
+  print("4th num +2nd num / 2nd num ${num4 + num2 + num2}");
+}
